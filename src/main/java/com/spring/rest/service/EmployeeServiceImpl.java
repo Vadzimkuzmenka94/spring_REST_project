bf64b0e6accc42b5ca15dfc87f7f375f406a6d76
37529 <VadzimKuzmenka@mail.ru>
2022-05-26 12:43:57 +0400

    Initial commit

---
+++ src/main/java/com/spring/rest/service/EmployeeServiceImpl.java
@@ -0,0 +1,41 @@
+package com.spring.rest.service;
+
+import com.spring.rest.dao.EmployeeDAO;
+import com.spring.rest.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployees() {
+        return employeeDAO.getAllEmployees();
+    }
+
+    @Transactional
+    @Override
+    public void saveEmployee(Employee employee) {
+        employeeDAO.saveEmployee(employee);
+    }
+
+    @Transactional
+    @Override
+    public Employee getEmployee(int id) {
+        return employeeDAO.getEmployee(id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteEmployee(int id) {
+        employeeDAO.deleteEmployee(id);
+
+    }
+}
